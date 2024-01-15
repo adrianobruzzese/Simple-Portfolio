84c23a2d466a974b7ae085a7cde6322a7204b153
Adriano Bruzzese <146066663+adrianobruzzese@users.noreply.github.com>
2024-01-15 22:48:07 +0100

    second-commit

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
   menu.classList.toggle('open');
   icon.classList.toggle('open');
 }
+
+// window.addEventListener(`scroll`,()=>{
+//   let scroll= window.scrollY;
+//    if (scroll>140){                        //quando la navbar scrolla oltre il banner di 455px aggiungiamo o togliamo la classe .downScroll (bg-color: white)
+//      header.classList.add(`downScroll`);
+//   }else{
+//      header.classList.remove(`downScroll`);
+//   }
+// })
+
